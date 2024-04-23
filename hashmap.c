bec6acd74d7ed99da7c90d81a84fd1e16b137870
benjyperedo <163452953+benjyperedo@users.noreply.github.com>
2024-04-23 13:03:54 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -91,10 +91,10 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
   long pos = 0;
-  if (map->buckets[pos]==NULL){
-    pos++;
-  }
   while(map->buckets[pos]!=NULL && map->buckets[pos]->key!=NULL){
+    if (map->buckets[pos]==NULL){
+      pos++;
+    }
     map->current = pos;
     return map->buckets[pos];
   }
